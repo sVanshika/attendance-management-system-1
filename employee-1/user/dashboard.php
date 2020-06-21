ab5630c8c9298a30c3dd1ad85f22c76c765fec42
Vanshika Sharma <48361763+sVanshika@users.noreply.github.com>
2020-06-21 23:04:14 +0530

    Update dashboard.php

--- employee-1/user/dashboard.php
+++ employee-1/user/dashboard.php
@@ -6,7 +6,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>attendance</title>
+    <title>BeingThere - Dashboard</title>
 
    <!--  Fontawesome  -->
    <link href="../fontawesome/css/all.css" rel="stylesheet"> 
